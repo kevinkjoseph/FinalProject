713307dc6f101c2e56a2576c2b1b58001988a8f2
Kevin Kuriakose Joseph <kevinkjoseph@Kevins-MacBook-Pro.local>
2018-11-29 19:18:53 +0400

    Added Structurs of the program

---
+++ dangerousdave.py
@@ -0,0 +1,27 @@
+add_library('minim')
+import os
+path = os.getcwd()
+player = Minim(this)
+
+class Creature:
+    def __init__(self,x,y,r,g,img,w,h,F):
+        self.x=x
+        self.y=y
+        self.r=r
+        
+
+def setup()
+pass
+
+
+def draw()
+pass
+
+"""
+1. Make Circle for the person
+2. Add velocties vx and vy and speeds and directions for him
+3. Add keyboards to make him move
+4. Add ground and the respective borders
+5. Make Game class
+6. Make person class
+7. Make obstacle class
